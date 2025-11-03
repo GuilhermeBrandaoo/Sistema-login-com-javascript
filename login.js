c35f5d64f507145c38a48baa10f2472a58796526
GuilhermeBrandaoo <desenvolvedorguilhermebrandao@gmail.com>
2025-11-03 19:17:29 -0300

    Correção

--- login.js
+++ login.js
@@ -3,21 +3,20 @@ const senhaCorreta = "1234";
 let tentativasMaximas = 3;
 let tentativasRestantes = 3;
 let i = 1;
-let usuario = prompt("Digite seu usuário:")
-let senha = prompt("Digite sua senha")
 
 do {
-    if (usuario === usuarioCorreto || senha === senhaCorreta) {
+    let usuario = prompt("Digite seu usuário:")
+    let senha = prompt("Digite sua senha")
+    if (usuario === usuarioCorreto && senha === senhaCorreta) {
         alert("Acesso permitido")
+        break;
     } else {
         tentativasRestantes = tentativasMaximas - i;
         i++
         alert("Acesso negado. Você só tem " +tentativasRestantes+ " tentativas")
-        let usuario = prompt("Digite seu usuário:")
-        let senha = prompt("Digite sua senha: ")
+        
+        if (i === tentativasMaximas) {
+            alert("Acesso bloqueado")
+        }
     }
 } while (i < tentativasMaximas);
-
-if (i = tentativasMaximas) {
-    alert("Acesso bloqueado")
-}
